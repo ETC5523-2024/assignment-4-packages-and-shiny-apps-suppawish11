a32bce2cf1c367d43fb5fbb05af14728769341dd
Suppawish Siriamnat <ssir0015@student.monash.edu>
2024-10-18 20:13:55 +1100

    change to shiny dashboard and custom color

--- data-raw/space-objects.R
+++ data-raw/space-objects.R
@@ -1,12 +1,10 @@
 ## code to prepare `space-objects` dataset goes here
 
-library(tidyverse)
-library(janitor)
 
 url <- 'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-04-23/outer_space_objects.csv'
 
-space_objects <- read_csv(url) |>
-  clean_names()
+space_objects <- readr::read_csv(url) |>
+  janitor::clean_names()
 
 
 
--- inst/app.R
+++ inst/app.R
@@ -1,103 +1,132 @@
 library(shiny)
 library(tidyverse)
 library(spaceobs)
-library(shinythemes)
 library(kableExtra)
 library(plotly)
+library(shinydashboard)
+
 
 # ui
-ui <- fillPage(
-  navbarPage("Space Launching", id = "top",
+ui <- dashboardPage(
+  dashboardHeader(title = span("Space Launching",
+                               style = "font-family: 'Courier New';
+                               font-weight: bold;")),
+  dashboardSidebar(
+    sidebarMenu(
+    menuItem("Overall ranking", tabName = "overall", icon = icon("ranking-star")),
+    menuItem("Launch count", icon = icon("rocket"), tabName = "launch")
+  )
+  ),
+  dashboardBody(
+    tags$head(tags$style(HTML(
 
-             theme = shinytheme("cerulean"),
+      '
 
-             # Overall ranking panel
-             tabPanel("Overall ranking",
+      .content {
+      background-color: #d8eef2;
+      }
 
-             fluidRow(
-               column(8,
-                      sliderInput(
-                        inputId = "year",
-                        label = "Year",
-                        min = min(space_objects$year),
-                        max = max(space_objects$year),
-                        value = 2023,
-                        sep = ""
-                      ))
-             ),
+      .skin-blue .main-sidebar {
+      background-color: #d8eef2;
+      font-family: Courier New;
+      font-weight: bold;
+      }
 
-             fluidRow(
-               column(5, mainPanel(
-                 htmlOutput("title_rank")
-                 )),
+      .skin-blue .main-sidebar .sidebar .sidebar-menu .active a{
+                                background-color: #31a8bd;
+                                }
 
-                column(7,
-                       mainPanel(
-                         htmlOutput("title_rankplot")
+      .skin-blue .main-sidebar .sidebar .sidebar-menu a{
+                                background-color: #d8eef2;
+                                color: #000000;
+                                }
+      '
+
+
+      ))),
+    tabItems(
+
+      tabItem(tabName = "overall",
+              fluidRow(
+                column(12,
+                       wellPanel(sliderInput(
+                         inputId = "year",
+                         label = "Year",
+                         min = min(space_objects$year),
+                         max = max(space_objects$year),
+                         value = 2023,
+                         sep = ""
+                       )))
+              ),
+
+              fluidRow(
+                box(
+                  title = htmlOutput("title_rank"), width = 6, solidHeader = TRUE, status = "primary",
+                  htmlOutput("ranking", height = 500)
+                ),
+
+                box(
+                  title = htmlOutput("title_rankplot"), width = 6, solidHeader = TRUE, status = "primary",
+                  plotlyOutput("rank_plot", height = 400)
+                )
+              )
+      ),
+
+
+      tabItem(tabName = "launch",
+              fluidRow(
+                column(6,
+                       wellPanel(selectizeInput(
+                         inputId = "entity",
+                         label = "Interested Entity?",
+                         choices = space_objects$entity
                        ))
-             ),
 
-             fluidRow(
-               style = "max-height: 60vh; overflow-y: auto;",
-
-               column(5,
-
-
-                      htmlOutput("ranking", height = 500)
-             ),
-              column(7,
-                    plotlyOutput("rank_plot", height = 400)
-                    )
-             )
-          ),
-
-          tabPanel("Launched count",
-                   fluidRow(
-                     column(6,
-                            selectInput(
-                              inputId = "entity",
-                              label = "Interested Entity?",
-                              choices = space_objects$entity
-                            )
-
-                     ),
-
-                     column(6,
-
-                            mainPanel(htmlOutput("totallatest")))
                   ),
 
-                  fluidRow(column(6,
-                                  mainPanel(
-                                    htmlOutput("title_time")
-                                  ))
-                           ),
+                column(6,
+                       valueBoxOutput("progressBox"))
+                ),
 
-                  fluidRow(
-                    style = "max-height: 60vh; overflow-y: auto;",
+              fluidRow(
+                box(
+                  title = htmlOutput("title_time"), width = 6, solidHeader = TRUE, status = "primary",
+                  plotlyOutput("cumuplot", height = 400)
+                ),
+
+                box(
+                  title = htmlOutput("title_yearplot"), width = 6, solidHeader = TRUE, status = "primary",
+                  plotlyOutput("yearplot", height = 400)
+                )
+              )
+              )
 
 
-                    column(6,
-                           plotlyOutput("cumuplot", height = 400)
-                           )
-                  )
-          )
-      )
-)
 
+              )
+
+    )
+  )
+
+
+# Server
 server <- function(input, output){
 
   #Title
   output$title_rank <- renderText(
-    "<h4><b>Overall objects launched ranking</b></h4>"
+    "<h4><b>Top 10 overall objects launched ranking in selected year</b></h4>"
   )
 
   output$title_rankplot <- renderText(
-    "<h4><b>Bar plot of overall ranking in selected year</b></h4>"
+    "<h4><b>Bar plot of top 10 overall ranking in selected year</b></h4>"
   )
 
   output$title_time <- renderText(
     "<h4><b>Cumulative objects launched of each entity</b></h4>"
+  )
+
+  output$title_yearplot <- renderText(
+    "<h4><b>Number of objects launched of each year</b></h4>"
   )
 
   # Overall ranking output
@@ -111,7 +140,7 @@ server <- function(input, output){
         group_by(entity) |>
         summarise(total = sum(num_objects)) |>
         arrange(-total) |>
-        head(8) |>
+        head(10) |>
         kable(col.names = c("Entity", "Total space objects launched")) |>
         kable_styling(bootstrap_options = c("striped", "hover"), full_width = FALSE)
     })
@@ -124,7 +153,7 @@ server <- function(input, output){
         group_by(entity) |>
         summarise(total = sum(num_objects)) |>
         arrange(-total) |>
-        head(8) |>
+        head(10) |>
         mutate(entity = fct_reorder(entity, -total)) |>
         ggplot(aes(x = entity, y = total)) +
         geom_col() +
@@ -139,16 +168,6 @@ server <- function(input, output){
   observe({
     req(input$entity)
 
-    #Total count latest year
-    output$totallatest <- renderText({
-      space_objects |>
-        filter(entity == input$entity) |>
-        group_by(entity) |>
-        summarize(total = sum(num_objects)) |>
-        kable(col.names = c("Entity", "Total objects launched")) |>
-        kable_styling(bootstrap_options = c("striped", "hover"), full_width = TRUE)
-    })
-
     # Cumulative count
     output$cumuplot <- renderPlotly({
 
@@ -159,11 +178,41 @@ server <- function(input, output){
       ggplot(aes(x = year,
                  y = cumulative_launch)) +
       geom_line(color = "red") +
-      geom_point(size = 0.5, color = "red") +
-      labs(x = "Year", y = "Total cumulative launched in each year"))
+      geom_point(size = 1, color = "red") +
+      labs(x = "Year", y = "Total cumulative launched each year") +
+        scale_x_continuous(breaks = seq(1957, 2023, by = 8),
+                           limits = c(1957, 2023)))
 
     })
+
+
+    output$yearplot <- renderPlotly({
+      ggplotly(
+        space_objects |>
+          filter(entity == input$entity) |>
+          ggplot(aes(x = year, y = num_objects)) +
+          geom_line(color = "red") +
+          geom_point(size = 1, color = "red") +
+          labs(x = "Year", y = "Number of objects launched each year") +
+          scale_x_continuous(breaks = seq(1957, 2023, by = 8),
+                             limits = c(1957, 2023))
+      )
+    })
+
+    output$progressBox <- renderValueBox({
+      valueBox(
+               space_objects |>
+                 filter(entity == input$entity) |>
+                 group_by(entity) |>
+                 summarize(total = sum(num_objects)) |>
+                 select(total) |>
+                 pull(total),
+               "Total objects launched",
+               icon = icon("rocket"),
+               color = "blue")})
   })
+
+
 
 
 }
