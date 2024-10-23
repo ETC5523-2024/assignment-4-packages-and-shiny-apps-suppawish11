5119c2f86347471f647dc2b8cd47b45cea6889dd
Suppawish Siriamnat <ssir0015@student.monash.edu>
2024-10-23 18:02:03 +1100

    document run-app

--- R/run-app.R
+++ R/run-app.R
@@ -1,9 +1,20 @@
-#' Title
+#' @title Launch a shiny app
 #'
-#' @return
-#' @export
+#' @description
+#' This calls the shiny app based on the space_object dataset with analysis shown inside.
+#'
+#'
+#' @return Return Shiny app
+#'
 #'
 #' @examples
+#' \dontrun{
+#' run_app()
+#' }
+#'
+#'
+#'
+#' @export
 run_app <- function() {
   app_dir <- system.file("app", package = "spaceobs")
   shiny::runApp(app_dir, display.mode = "normal")
