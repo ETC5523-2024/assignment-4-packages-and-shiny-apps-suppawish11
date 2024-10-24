44fb17a022edb676769523de5eb3892a52a457d0
Suppawish Siriamnat <ssir0015@student.monash.edu>
2024-10-24 22:38:19 +1100

    change package documentation

--- R/spaceobs-package.R
+++ R/spaceobs-package.R
@@ -1,9 +1,6 @@
-#' spaceobs Package
-#'
-#' @description
-#' This package contain a yearly number of objects launched into outer space dataset
-#' with shiny dashboard app showing the analysis of this dataset.
-#' Learn more about `spaceobs` from the vignette: `browseVignettes(package = "spaceobs")`
-
-
+#' @keywords internal
 "_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
